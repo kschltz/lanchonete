402a553ab54c1944434733b989eb4353f9a71202
Caio Santos <caio.santos@piposaude.com.br>
2023-10-10 11:01:23 -0300

    init and run migratus on run app

--- src/mba_fiap/lanchonete.clj
+++ src/mba_fiap/lanchonete.clj
@@ -1,12 +1,21 @@
 (ns mba-fiap.lanchonete
-  (:gen-class))
+  (:gen-class)
+  (:require
+    [aero.core :as aero]
+    [mba-fiap.datasource.migratus]
+    [clojure.java.io :as io]
+    [integrant.core :as ig]))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+(def ^:const system-filename "config.edn")
+(defn read-config []
+  (aero/read-config (io/resource system-filename)))
+
+(defn start-app []
+  (let [config-map (read-config)]
+    (ig/prep config-map)
+    (ig/init config-map)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (greet {:name (first args)}))
+  (start-app))
