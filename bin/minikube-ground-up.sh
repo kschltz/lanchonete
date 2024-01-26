121e847b170d348692e4b6de3ab139a1e14d61a0
Kaue Schultz <kaue.schultz@beacon.works>
2024-01-25 22:17:35 -0300

    ADD:loadbalancer + HPA

--- bin/minikube-ground-up.sh
+++ bin/minikube-ground-up.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-alias kubectl='minikube kubectl --' && \
-minikube delete && \
-minikube start && \
-CREDS_PATH=~/.docker/config.json bash ./bin/docker-secrets.sh && \
-minikube kubectl -- apply -f ./k8s/postgre/ && \
-minikube kubectl -- apply -f ./k8s/postgre/ && \
-minikube kubectl -- apply -f ./k8s/lanchonete/  && \
-minikube kubectl -- logs -l app=lanchonete  -f
+alias kubectl='minikube kubectl --' &&
+  minikube delete &&
+  minikube start
+
+CREDS_PATH=~/.docker/config.json bash ./bin/docker-secrets.sh &&
+  minikube kubectl -- apply -f ./k8s/postgre/ &&
+  minikube kubectl -- apply -f ./k8s/postgre/ &&
+  minikube kubectl -- apply -f ./k8s/lanchonete/ &&
+  minikube kubectl -- logs -l app=lanchonete -f  && minikube addons enable metrics-server
