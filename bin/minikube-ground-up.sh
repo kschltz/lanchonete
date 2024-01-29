e8f59f7ee4ca5d663afc033a2be7b118540c58d5
Kaue Schultz <kaue.schultz@beacon.works>
2024-01-29 19:01:33 -0300

    fix scripts

--- bin/minikube-ground-up.sh
+++ bin/minikube-ground-up.sh
@@ -5,7 +5,7 @@ minikube start
 wait
 minikube image build -t lanchonete .
 wait
-CREDS_PATH=~/.docker/config.json bash ./bin/docker-secrets.sh
+#CREDS_PATH=~/.docker/config.json bash ./bin/docker-secrets.sh
 minikube kubectl -- apply -f ./k8s/postgre/
 minikube kubectl -- apply -f ./k8s/lanchonete/
 minikube kubectl -- logs -l app=lanchonete -f
