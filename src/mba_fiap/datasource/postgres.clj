d6b6a3f9941c4c3e1ba2090da74179d06f057b8c
Caio Santos <caio.santos687@gmail.com>
2024-03-18 12:50:40 -0300

    improve get-hostname function

--- src/mba_fiap/datasource/postgres.clj
+++ src/mba_fiap/datasource/postgres.clj
@@ -3,14 +3,21 @@
             [integrant.core :as ig]
             [next.jdbc :as jdbc]))
 
-(defn ^:private get-host [host-port]
-  (first (str/split host-port #":")))
+(defn ^:private ensure-http-protocol [url]
+  (if (or (str/starts-with? url "http://")
+          (str/starts-with? url "https://"))
+    url
+    (str "http://" url)))
+
+(defn ^:private get-hostname [host-port]
+  (let [endpoint (ensure-http-protocol host-port)]
+    (.getHost (java.net.URL. endpoint))))
 
 (defmethod ig/init-key ::db
   [_ {:keys [spec]
       :as component}]
   (println "Initializing database connection: " component)
-  (let [updated-spec (update spec :host get-host)]
+  (let [updated-spec (update spec :host get-hostname)]
     (assoc
       component
       :datasource
