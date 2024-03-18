0e0d85fc8b47dcecd0cda14496b3d3af496fbdd9
Caio Santos <caio.santos687@gmail.com>
2024-03-18 12:16:46 -0300

    split host and port in db init-key

--- src/mba_fiap/datasource/postgres.clj
+++ src/mba_fiap/datasource/postgres.clj
@@ -1,16 +1,20 @@
 (ns mba-fiap.datasource.postgres
-  (:require [clojure.tools.logging :as log]
+  (:require [clojure.string :as str]
             [integrant.core :as ig]
             [next.jdbc :as jdbc]))
+
+(defn ^:private get-host [host-port]
+  (first (str/split host-port #":")))
 
 (defmethod ig/init-key ::db
   [_ {:keys [spec]
       :as component}]
   (println "Initializing database connection: " component)
-  (assoc
-    component
-    :datasource
-    (jdbc/get-datasource spec )))
+  (let [updated-spec (update spec :host get-host)]
+    (assoc
+      component
+      :datasource
+      (jdbc/get-datasource updated-spec))))
 
 (defmethod ig/resolve-key ::db
   [_ {:keys [datasource]}]
