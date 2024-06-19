6e4b113f3a3bd232c3861220b91fe4f4a4d69851
caio santos <caiocss@outlook.com>
2024-06-18 22:37:06 -0300

    wip - remove client data endpoint

---
+++ src/mba_fiap/usecase/cliente.clj
@@ -0,0 +1,19 @@
+(ns mba-fiap.usecase.cliente
+  (:require
+   [mba-fiap.base.validation :as validation]
+   [mba-fiap.service.cliente :as cliente-service]
+   [mba-fiap.value-object.cpf :as cpf])
+  (:import [mba_fiap.repository.repository Repository]))
+
+
+(defn excluir-cliente [^Repository repository cpf]
+  {:pre [(instance? Repository repository)
+         (validation/schema-check cpf/CPF cpf)]}
+   (let [cliente (cliente-service/buscar-por-cpf repository cpf)]
+    (if (empty? cliente)
+      {:status :not-found
+       :cliente cliente}
+      (do
+        (cliente-service/remover-cliente repository (:id cliente))
+        {:status :success
+         :cliente cliente}))))
